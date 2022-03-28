0f6095a6d90d10c8bbeac61a2a61562ac1a171e2
Mirian Dayane Cohls de Amorim <85651142+MirianAmorim@users.noreply.github.com>
2022-03-27 22:17:57 -0300

    aulaTeste
    Signed-off-by: Mirian Dayane Cohls de Amorim <85651142+MirianAmorim@users.noreply.github.com>

--- aulaTeste.js
+++ aulaTeste.js
@@ -2,3 +2,28 @@
 
 // const numeros = [1, 4, 9];
 
+// const raiz = numeros.map (Math.sqrt);
+// console.log(raiz);
+// const dobro = numeros.map( (num) => {
+//     return (num) * 2;
+// });
+// console.log(dobro);
+const material = [
+    {nome: "Lápis", valor: 2 },
+    {nome: "Caderno", valor: 15 },
+    {nome: "Caneta", valor: 7 },
+    {nome: "Agenda", valor: 18 },
+    {nome: "Régua", valor: 3 },
+    {nome: "Borracha", valor: 1 },
+];
+
+const inflacao = material.map( (item) => {
+    return (item.valor) * 1.50;
+});
+console.log(inflacao);
+console.log(material);
+console.table(material);
+
+
+
+
