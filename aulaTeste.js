f2d6c0bfff0d44575b4e64de75086757ebb107b0
Mirian Dayane Cohls de Amorim <85651142+MirianAmorim@users.noreply.github.com>
2022-03-27 21:09:07 -0300

    aulaTeste
    Signed-off-by: Mirian Dayane Cohls de Amorim <85651142+MirianAmorim@users.noreply.github.com>

--- aulaTeste.js
+++ aulaTeste.js
@@ -1,12 +1,4 @@
-// *** Método MAP ***
+// // *** Método MAP ***
 
-const numeros = [1, 4, 9];
-const raiz = numeros.map (Math.sqrt);
-// const dobro = numeros.map( num => {
-//     return num * 2;
-// });
+// const numeros = [1, 4, 9];
 
-
-// console.log(dobro);
-// console.log(raiz);
-// console.log(numeros);
